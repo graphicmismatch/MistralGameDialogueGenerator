2df0850529060e3ebbdaaf45e85cd013479a5b65
Yash <yashpree237915@gmail.com>
2025-06-19 00:20:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from src. GameReviewerPromptBuilder import  GameReviewerPromptBuilder
 builder = GameReviewerPromptBuilder("reviewer.json")
 game_summary = "A pixel-art platformer where a frog battles capitalist robots to reclaim his swamp."
 print(builder.build_prompt(game_summary))
