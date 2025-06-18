139328253a3a2a4716a39b20f75480cf7c88db2b
Yash <yashpree237915@gmail.com>
2025-06-19 00:18:51 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,34 +1,3 @@
-from src.InferenceEngine import InferenceEngine, FSLData
-
-
-# inference engine setup
-ie = InferenceEngine(temp=0.1)
-
-
-# Example Callback
-def testingres(res):
-    print(res.choices[0].message.content)
-
-
-# MultiShot/FewShot Learning setup
-examples = []
-
-examples.append(
-    FSLData(
-        "Say Hello World in a random language.",
-        "1) Language: translation",
-    )
-)
-examples.append(
-    FSLData(
-        "Say Hello World in 2 random languages.",
-        "1) English: smtg smtg\n2) French: smtg smtg",
-    )
-)
-
-
-ie.GivePrompt(
-    testingres,
-    "Say Hello World in 12 random languages.",
-    learning=examples,
-)
+builder = GameReviewerPromptBuilder("reviewer.json")
+game_summary = "A pixel-art platformer where a frog battles capitalist robots to reclaim his swamp."
+print(builder.build_prompt(game_summary))
